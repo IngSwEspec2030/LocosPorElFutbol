a214a839959a6ebb6a952581ecf4513f13897ab9
Rigo <rigoxls@gmail.com>
2020-09-04 17:08:27 -0500

    Ajuste calendario multiple a simple

--- frontEnd/project/src/app/components/pages/home-one/home-one.component.ts
+++ frontEnd/project/src/app/components/pages/home-one/home-one.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {NgbDate, NgbCalendar, NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 import {HomeService} from './home.service';
 
 @Component({
@@ -8,16 +8,11 @@ import {HomeService} from './home.service';
     styleUrls: ['./home-one.component.scss']
 })
 export class HomeOneComponent implements OnInit {
-
-    hoveredDate: NgbDate | null = null;
-    fromDate: NgbDate | null;
-    toDate: NgbDate | null;
+    model: NgbDateStruct;
 
     activities: any;
 
-    constructor(private calendar: NgbCalendar,
-                public formatter: NgbDateParserFormatter,
-                private homeService: HomeService) {
+    constructor(private homeService: HomeService) {
 
     }
 
@@ -28,32 +23,5 @@ export class HomeOneComponent implements OnInit {
 
     }
 
-    onDateSelection(date: NgbDate) {
-        if (!this.fromDate && !this.toDate) {
-            this.fromDate = date;
-        } else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
-            this.toDate = date;
-        } else {
-            this.toDate = null;
-            this.fromDate = date;
-        }
-    }
-
-    isHovered(date: NgbDate) {
-        return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
-    }
-
-    isInside(date: NgbDate) {
-        return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
-    }
-
-    isRange(date: NgbDate) {
-        return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
-    }
-
-    validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
-        const parsed = this.formatter.parse(input);
-        return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
-    }
 
 }
