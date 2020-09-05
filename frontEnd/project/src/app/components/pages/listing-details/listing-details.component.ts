92de01f4271234152c6b5fa1a7e736715114626c
rodri181292 <rodrigo2bastidas_@hotmail.com>
2020-09-05 16:51:30 -0500

    cambios en la vista de detalle

--- frontEnd/project/src/app/components/pages/listing-details/listing-details.component.ts
+++ frontEnd/project/src/app/components/pages/listing-details/listing-details.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-listing-details',
   templateUrl: './listing-details.component.html',
@@ -7,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor( ) {
+    
+   }
 
   ngOnInit(): void {
   }
