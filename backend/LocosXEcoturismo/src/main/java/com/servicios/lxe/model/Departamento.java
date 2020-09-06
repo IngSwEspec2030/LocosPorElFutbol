1ed6bef59749bba18d8ee0eac8f0ace8c0ad686e
natalia-Leon <leonc.natalia@gmail.com>
2020-09-06 18:56:08 -0500

    23697 - Middle_No permite autenticación_solicita next token

---
+++ backend/LocosXEcoturismo/src/main/java/com/servicios/lxe/model/Departamento.java
@@ -0,0 +1,35 @@
+package com.servicios.lxe.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Departamento implements Serializable{
+
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id_departamento;
+	@Column(name = "nombreDepartamento")
+	private String nombreDepartamento;
+	
+	
+	public int getId_departamento() {
+		return id_departamento;
+	}
+	public void setId_departamento(int id_departamento) {
+		this.id_departamento = id_departamento;
+	}
+	public String getNombreDepartamento() {
+		return nombreDepartamento;
+	}
+	public void setNombreDepartamento(String nombreDepartamento) {
+		this.nombreDepartamento = nombreDepartamento;
+	}
+	
+}
---
+++ backend/LocosXEcoturismo/src/main/java/com/servicios/lxe/model/SitioTuristico.java
@@ -0,0 +1,79 @@
+package com.servicios.lxe.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+
+@Entity
+public class SitioTuristico implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id_sitio;
+	@Column(name = "nombreSitio")
+	private String nombreSitio;
+	@Column(name = "descripcion")
+	private String descripcion;
+	@OneToOne
+	private Municipio idMunicipio;
+	@Column(name = "idLatitud")
+	private long idLatitud;
+	@Column(name = "idLongitud")
+	private long idLongitud;
+	@OneToMany(targetEntity=Imagen.class, mappedBy="sitioTuristico",cascade=CascadeType.ALL, fetch = FetchType.LAZY)	   
+	private List<Imagen> imagen;
+	
+	public Long getId_sitio() {
+		return id_sitio;
+	}
+	public void setId_sitio(Long id_sitio) {
+		this.id_sitio = id_sitio;
+	}
+	public String getNombreSitio() {
+		return nombreSitio;
+	}
+	public void setNombreSitio(String nombreSitio) {
+		this.nombreSitio = nombreSitio;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Municipio getIdMunicipio() {
+		return idMunicipio;
+	}
+	public void setIdMunicipio(Municipio idMunicipio) {
+		this.idMunicipio = idMunicipio;
+	}
+	public long getIdLatitud() {
+		return idLatitud;
+	}
+	public void setIdLatitud(long idLatitud) {
+		this.idLatitud = idLatitud;
+	}
+	public long getIdLongitud() {
+		return idLongitud;
+	}
+	public void setIdLongitud(long idLongitud) {
+		this.idLongitud = idLongitud;
+	}
+	public List<Imagen> getImagen() {
+		return imagen;
+	}
+	public void setImagen(List<Imagen> imagen) {
+		this.imagen = imagen;
+	}
+	
+}
