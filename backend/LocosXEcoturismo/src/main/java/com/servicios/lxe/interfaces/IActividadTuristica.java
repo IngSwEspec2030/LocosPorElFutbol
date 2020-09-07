f006d3dfc1814789bf717442f39af15d294712e4
natalia-Leon <leonc.natalia@gmail.com>
2020-09-07 18:01:21 -0500

    Eliminación tabla Paquete.

---
+++ backend/LocosXEcoturismo/src/main/java/com/servicios/lxe/interfaces/IActividadTuristica.java
@@ -0,0 +1,10 @@
+package com.servicios.lxe.interfaces;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.servicios.lxe.model.ActividadTuristica;
+
+
+public interface IActividadTuristica extends JpaRepository<ActividadTuristica, Integer>{
+
+}
--- backend/LocosXEcoturismo/src/main/java/com/servicios/lxe/model/ActividadTuristica.java
+++ backend/LocosXEcoturismo/src/main/java/com/servicios/lxe/model/ActividadTuristica.java
@@ -1,12 +1,14 @@
 package com.servicios.lxe.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class ActividadTuristica implements Serializable{
@@ -16,6 +18,18 @@ public class ActividadTuristica implements Serializable{
 	private int id_actividad;
 	@Column(name = "nombreActividad")
 	private String nombreActividad;
+	@Column(name = "descripcion")
+	private String descripcion;
+	@Column(name = "categoria")
+	private String categoria;
+	@Column(name = "precioBase")
+	private BigDecimal precioBase;
+	@Column(name = "review")
+	private Integer review;
+	@Column(name = "estado")
+	private Integer estado;
+	@OneToOne
+	private SitioTuristico sitioTuristico;
 	
 	public int getId_actividad() {
 		return id_actividad;
@@ -29,6 +43,41 @@ public class ActividadTuristica implements Serializable{
 	public void setNombreActividad(String nombreActividad) {
 		this.nombreActividad = nombreActividad;
 	}
-	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public BigDecimal getPrecioBase() {
+		return precioBase;
+	}
+	public void setPrecioBase(BigDecimal precioBase) {
+		this.precioBase = precioBase;
+	}
+	public Integer getReview() {
+		return review;
+	}
+	public void setReview(Integer review) {
+		this.review = review;
+	}
+	public Integer getEstado() {
+		return estado;
+	}
+	public void setEstado(Integer estado) {
+		this.estado = estado;
+	}
+	public SitioTuristico getSitioTuristico() {
+		return sitioTuristico;
+	}
+	public void setSitioTuristico(SitioTuristico sitioTuristico) {
+		this.sitioTuristico = sitioTuristico;
+	}
 	
 }
