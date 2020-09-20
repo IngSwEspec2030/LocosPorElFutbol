09899662e1fb9911d9bf0a1c66a7e930e70a9814
Rigo <rigoxls@gmail.com>
2020-09-20 01:17:10 -0500

    Metodos front y back para creación de actividades

---
+++ backEnd/LocosXEcoturismo/src/main/java/com/servicios/lxe/service/ServicioActividad.java
@@ -0,0 +1,28 @@
+package com.servicios.lxe.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.servicios.lxe.dto.ActividadDto;
+import com.servicios.lxe.interfaces.IActividadTuristica;
+import com.servicios.lxe.model.ActividadTuristica;
+
+
+@Service
+public class ServicioActividad {
+	
+	@Autowired
+	private IActividadTuristica iActividadTuristica;
+	
+	protected ServicioActividad(){
+		
+	} 
+	
+	public ActividadTuristica createActivity(ActividadDto newActivity) {
+		ActividadTuristica activity = new ActividadTuristica(newActivity);
+		System.out.println(activity);
+		iActividadTuristica.save(activity);
+		return activity;
+	}	
+
+}
