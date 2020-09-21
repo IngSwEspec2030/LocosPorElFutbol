258c7845a068ad741896a12aa11f5ef9397cd526
Rigo <rigoxls@gmail.com>
2020-09-20 19:26:26 -0500

    Integración de popup de eliminación actividades

--- frontEnd/admin/src/app/components/pages/list-activity/list-activity.component.ts
+++ frontEnd/admin/src/app/components/pages/list-activity/list-activity.component.ts
@@ -1,16 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {ListActivityService} from './list-activity.service';
+import {NgbModalConfig, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
     selector: 'app-list-activity',
     templateUrl: './list-activity.component.html',
-    styleUrls: ['./list-activity.component.scss']
+    styleUrls: ['./list-activity.component.scss'],
+    providers: [NgbModalConfig, NgbModal]
 })
 export class ListActivityComponent implements OnInit {
 
     public activities = [];
 
-    constructor(private listActivityService: ListActivityService) {
+    constructor(
+        private listActivityService: ListActivityService,
+        private modalService: NgbModal) {
         this.getActivities();
     }
 
@@ -19,8 +24,12 @@ export class ListActivityComponent implements OnInit {
 
     async getActivities() {
         this.activities = await this.listActivityService.getActivities();
-        this.activities.sort( (a, b) => {
+        this.activities.sort((a, b) => {
             return (a['nombreActividad'] > b['nombreActividad']) ? 1 : -1;
         });
+    }
+
+    open(content) {
+        this.modalService.open(content);
     }
 }
