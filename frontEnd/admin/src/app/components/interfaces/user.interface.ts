4b83cb9eb54d99ec735e5d4a24ee99be0b4e7df9
Rigo <rigoxls@gmail.com>
2020-09-25 08:46:54 -0500

    upsert user

---
+++ frontEnd/admin/src/app/components/interfaces/user.interface.ts
@@ -0,0 +1,15 @@
+export interface UserInterface {
+    tipoUsuario: number;
+    nombres: string;
+    apellidos: string;
+    identificacion: string;
+    email: string;
+    password: string;
+    telefono: string;
+    idRole: number;
+    nombreProveedor: string;
+    nombreRepresentante: string;
+    idUsuario?: number;
+    idProveedor?: number;
+    idRole: number;
+}
--- frontEnd/admin/src/app/components/pages/user/user.service.ts
+++ frontEnd/admin/src/app/components/pages/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {UserInterface} from '../../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,39 @@ export class UserService {
     ) {
     }
 
-    // TODO ALL
+    upsertActivity(user: UserInterface, userId?: number) {
+        return new Promise((resolve, reject) => {
+            const userSave = user;
+            const path = (userId) ? 'user/update' : 'user/create';
+
+            const headers = {
+                'Content-Type': 'application/json'
+            };
+
+            const formData = {
+                tipoUsuario: parseInt(String(userSave.tipoUsuario), 10),
+                nombres: userSave.nombres,
+                apellidos: userSave.apellidos,
+                identificacion: userSave.identificacion,
+                email: userSave.email,
+                password: userSave.password,
+                telefono: userSave.telefono,
+                idRole: parseInt(String(userSave.tipoUsuario), 10),
+                nombreProveedor: userSave.nombreProveedor,
+                nombreRepresentante: userSave.nombreRepresentante,
+            };
+
+            if (userId) {
+                formData['idUsuario'] = userId;
+                formData['idProveedor'] = userSave.idProveedor;
+            }
+
+            return this.http.post<any>(`${this.baseUrl}${path}`, formData, {headers}).subscribe(response => {
+                    resolve({status: 201});
+                },
+                error => {
+                    reject(error);
+                });
+        });
+    }
 }
